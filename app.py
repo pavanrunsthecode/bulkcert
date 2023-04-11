ec47a38e3c1eae6703a85e6ebc81e3ae8cf17c03
pavanrunsthecode <pavango1testing@gmail.com>
2023-04-11 13:20:00 +1000

    ready for deployment

--- app.py
+++ app.py
@@ -45,4 +45,5 @@ def submit_form():
         #return send_file(zip_filename, mimetype='zip', as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # to run locally app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
